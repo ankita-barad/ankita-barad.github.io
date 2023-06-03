c6698b4fab8e2bf2836748ee28e5d2aa65cd4de8
ankita-barad <ankitabarad2111@gmail.com>
2023-06-03 20:00:28 +0530

    mark-21

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("resume-button-1").addEventListener("click", () => {
+  window.open(
+    "https://drive.google.com/file/d/1lVQeTKDmLPowzpq9ToLy0H2qhgEbSatP/view?usp=sharing",
+    "_blank"
+  );
+});
+
+document.getElementById("resume-button-2").addEventListener("click", () => {
+  window.open(
+    "https://drive.google.com/file/d/1lVQeTKDmLPowzpq9ToLy0H2qhgEbSatP/view?usp=sharing",
+    "_blank"
+  );
+});
